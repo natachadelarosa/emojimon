11c5572dcdbf71244efdaba65b454fedced6dde6
Kevin Ingersoll <kingersoll@gmail.com>
2023-05-17 16:45:15 -0700

    Step 3: A wild emojimon appears

--- packages/contracts/src/systems/MapSystem.sol
+++ packages/contracts/src/systems/MapSystem.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 import { System } from "@latticexyz/world/src/System.sol";
-import { MapConfig, Movable, Obstruction, Player, Position } from "../codegen/Tables.sol";
+import { Encounter, Encounterable, EncounterTrigger, MapConfig, Monster, Movable, Obstruction, Player, Position } from "../codegen/Tables.sol";
+import { MonsterType } from "../codegen/Types.sol";
 import { addressToEntityKey } from "../addressToEntityKey.sol";
 import { positionToEntityKey } from "../positionToEntityKey.sol";
 
@@ -21,6 +22,7 @@ contract MapSystem is System {
     Player.set(player, true);
     Position.set(player, x, y);
     Movable.set(player, true);
+    Encounterable.set(player, true);
   }
 
   function move(uint32 x, uint32 y) public {
@@ -29,6 +31,9 @@ contract MapSystem is System {
 
     (uint32 fromX, uint32 fromY) = Position.get(player);
     require(distance(fromX, fromY, x, y) == 1, "can only move to adjacent spaces");
+
+    uint256 actionCount = Encounter.getActionCount(player);
+    require(actionCount == 0, "cannot move during an encounter");
 
     bytes32 position = positionToEntityKey(x, y);
     require(!Obstruction.get(position), "this space is obstructed");
@@ -39,11 +44,25 @@ contract MapSystem is System {
     y = y + (height % height);
 
     Position.set(player, x, y);
+
+    if (Encounterable.get(player) && EncounterTrigger.get(position)) {
+      uint256 rand = uint256(keccak256(abi.encode(player, position, blockhash(block.number - 1), block.difficulty)));
+      if (rand % 5 == 0) {
+        startEncounter(player);
+      }
+    }
   }
 
   function distance(uint32 fromX, uint32 fromY, uint32 toX, uint32 toY) internal pure returns (uint32) {
     uint32 deltaX = fromX > toX ? fromX - toX : toX - fromX;
     uint32 deltaY = fromY > toY ? fromY - toY : toY - fromY;
     return deltaX + deltaY;
+  }
+
+  function startEncounter(bytes32 player) internal {
+    bytes32 monster = keccak256(abi.encode(player, blockhash(block.number - 1), block.difficulty));
+    MonsterType monsterType = MonsterType((uint256(monster) % uint256(type(MonsterType).max)) + 1);
+    Monster.set(monster, monsterType);
+    Encounter.set(player, 1, monster);
   }
 }
